d8fe70f861475e1cc6bae5e5146ec17c8bdf6358
Dongbin Han <unimunity@code.do>
2024-11-25 02:08:16 +0100

    Add fugitive

---
+++ nvim/lua/plugins/git.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    "tpope/vim-fugitive",
+    -- keys = {
+    --   "<leader>fb",
+    --   ":Git blane<CR>",
+    --   { desc = "Blame" },
+    --   "<leader>fs",
+    --   ":Git<CR>",
+    --   { desc = "Status" },
+    --   "<leader>fw",
+    --   ":Git show<CR>",
+    --   { desc = "Show" },
+    --   "<leader>fl",
+    --   ":Git log --decorate=short<CR>",
+    --   { desc = "Log" },
+    --   "<leader>ff",
+    --   ":Git log --decorate=short -p %<CR>",
+    --   { desc = "File log" },
+    --   "<leader>f1",
+    --   ":Git log --oneline<CR>",
+    --   { desc = "Log oneline" },
+    -- },
+  },
+}
